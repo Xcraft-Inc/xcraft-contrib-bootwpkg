981c326c95b39306e1db1aef2eaa77437890e7ad
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-04 09:28:48 +0100

    Merge branch 'xcraft'

--- wpkg.js
+++ wpkg.js
@@ -18,6 +18,8 @@ var cmd = {};
 var makeRun = function (callback) {
   zogLog.info ('begin building of wpkg');
 
+  process.env.SHELL = cmd.exe;
+
   var os = require ('os');
   var list = [
     'all',
