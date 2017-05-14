a55a92d2c7e151f0ebc1862a6a384ea6c56aa2f9
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-14 10:14:44 +0200

    Adapt for new commands discovering

--- wpkg.js
+++ wpkg.js
@@ -237,9 +237,12 @@ cmd.build = function (msg, response) {
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function () {
-  const xUtils = require ('xcraft-core-utils');
   return {
     handlers: cmd,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
+    rc: {
+      build: {
+        desc: 'build and install WPKG',
+      },
+    },
   };
 };
