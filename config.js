656def9659e94adcfd11496817df6327e1197f68
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-14 10:13:58 +0200

    Cosmetic: fix formatting

--- config.js
+++ config.js
@@ -3,24 +3,29 @@
 /**
  * Retrieve the inquirer definition for xcraft-core-etc.
  */
-module.exports = [{
-  type: 'input',
-  name: 'name',
-  message: 'package name',
-  default: 'unigw'
-}, {
-  type: 'input',
-  name: 'version',
-  message: 'version',
-  default: '1.0.2'
-}, {
-  type: 'input',
-  name: 'src',
-  message: 'source URI',
-  default: 'https://github.com/Skywalker13/unigw/archive/cf58947c03a304e67a2f283ca1943d0ed3b898d5.tar.gz'
-}, {
-  type: 'input',
-  name: 'out',
-  message: 'output directory',
-  default: './usr'
-}];
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'package name',
+    default: 'unigw',
+  },
+  {
+    type: 'input',
+    name: 'version',
+    message: 'version',
+    default: '1.0.2',
+  },
+  {
+    type: 'input',
+    name: 'src',
+    message: 'source URI',
+    default: 'https://github.com/Skywalker13/unigw/archive/cf58947c03a304e67a2f283ca1943d0ed3b898d5.tar.gz',
+  },
+  {
+    type: 'input',
+    name: 'out',
+    message: 'output directory',
+    default: './usr',
+  },
+];
--- wpkg.js
+++ wpkg.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var path  = require ('path');
+var path = require ('path');
 var async = require ('async');
 
-const xCMake    = require ('xcraft-contrib-bootcmake');
+const xCMake = require ('xcraft-contrib-bootcmake');
 const xPlatform = require ('xcraft-core-platform');
-const xFs       = require ('xcraft-core-fs');
-
+const xFs = require ('xcraft-core-fs');
 
 var cmd = {};
 
-
 /* TODO: must be generic. */
 var makeRun = function (makeDir, response, callback) {
-  const pkgConfig = require ('xcraft-core-etc') (null, response).load ('xcraft-contrib-bootwpkg');
+  const pkgConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft-contrib-bootwpkg'
+  );
 
   response.log.info ('begin building of wpkg');
 
@@ -24,34 +24,36 @@ var makeRun = function (makeDir, response, callback) {
   }
 
   var os = require ('os');
-  var list = [
-    'all'
-  ];
+  var list = ['all'];
 
   const xProcess = require ('xcraft-core-process') ({
     logger: 'xlog',
     parser: 'cmake',
-    resp:   response
+    resp: response,
   });
 
   var currentDir = process.cwd ();
   process.chdir (makeDir);
-  async.eachSeries (list, function (args, callback) {
-    var fullArgs = ['-j' + os.cpus ().length].concat (args);
+  async.eachSeries (
+    list,
+    function (args, callback) {
+      var fullArgs = ['-j' + os.cpus ().length].concat (args);
 
-    xProcess.spawn (make, fullArgs, {}, function (err) {
-      callback (err ? 'make failed: ' + err : null);
-    });
-  }, function (err) {
-    process.chdir (currentDir);
+      xProcess.spawn (make, fullArgs, {}, function (err) {
+        callback (err ? 'make failed: ' + err : null);
+      });
+    },
+    function (err) {
+      process.chdir (currentDir);
 
-    if (!err) {
-      xFs.cp (path.join (makeDir, 'dist/usr'), path.resolve (pkgConfig.out));
-      response.log.info ('wpkg is built and installed');
+      if (!err) {
+        xFs.cp (path.join (makeDir, 'dist/usr'), path.resolve (pkgConfig.out));
+        response.log.info ('wpkg is built and installed');
+      }
+
+      callback (err ? 'make failed' : null);
     }
-
-    callback (err ? 'make failed' : null);
-  });
+  );
 };
 
 /* TODO: must be generic. */
@@ -62,10 +64,7 @@ var cmakeRun = function (srcDir, response, callback) {
   var buildDir = path.join (srcDir, '../BUILD_WPKG');
   xFs.mkdir (buildDir);
 
-  var args = [
-    '-DCMAKE_COLOR_MAKEFILE=OFF',
-    '-DCMAKE_BUILD_TYPE=Release'
-  ];
+  var args = ['-DCMAKE_COLOR_MAKEFILE=OFF', '-DCMAKE_BUILD_TYPE=Release'];
 
   if (xPlatform.getOs () === 'win') {
     args.unshift ('-G', 'MinGW Makefiles');
@@ -76,7 +75,7 @@ var cmakeRun = function (srcDir, response, callback) {
   const xProcess = require ('xcraft-core-process') ({
     logger: 'xlog',
     parser: 'cmake',
-    resp:   response
+    resp: response,
   });
 
   var currentDir = process.cwd ();
@@ -89,7 +88,7 @@ var cmakeRun = function (srcDir, response, callback) {
 
 var patchRun = function (srcDir, response, callback) {
   var xDevel = require ('xcraft-core-devel');
-  var async  = require ('async');
+  var async = require ('async');
 
   var os = xPlatform.getOs ();
 
@@ -101,91 +100,135 @@ var patchRun = function (srcDir, response, callback) {
     return;
   }
 
-  async.eachSeries (list, function (file, callback) {
-    response.log.info ('apply patch: ' + file);
-    var patchFile = path.join (patchDir, file);
+  async.eachSeries (
+    list,
+    function (file, callback) {
+      response.log.info ('apply patch: ' + file);
+      var patchFile = path.join (patchDir, file);
 
-    xDevel.patch (srcDir, patchFile, 1, response, function (err) {
-      callback (err ? 'patch failed: ' + file + ' ' + err : null);
-    });
-  }, function (err) {
-    callback (err);
-  });
+      xDevel.patch (srcDir, patchFile, 1, response, function (err) {
+        callback (err ? 'patch failed: ' + file + ' ' + err : null);
+      });
+    },
+    function (err) {
+      callback (err);
+    }
+  );
 };
 
 /**
  * Build the wpkg package.
  */
 cmd.build = function (msg, response) {
-  const xcraftConfig = require ('xcraft-core-etc') (null, response).load ('xcraft');
-  const pkgConfig    = require ('xcraft-core-etc') (null, response).load ('xcraft-contrib-bootwpkg');
+  const xcraftConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft'
+  );
+  const pkgConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft-contrib-bootwpkg'
+  );
   var xEnv = require ('xcraft-core-env');
 
   var archive = path.basename (pkgConfig.src);
-  var inputFile  = pkgConfig.src;
+  var inputFile = pkgConfig.src;
   var outputFile = path.join (xcraftConfig.tempRoot, 'src', archive);
 
-  async.auto ({
-    taskHttp: function (callback) {
-      var xHttp = require ('xcraft-core-http');
+  async.auto (
+    {
+      taskHttp: function (callback) {
+        var xHttp = require ('xcraft-core-http');
 
-      xHttp.get (inputFile, outputFile, function () {
-        callback ();
-      }, function (progress, total) {
-        response.log.progress ('Downloading', progress, total);
-      });
-    },
+        xHttp.get (
+          inputFile,
+          outputFile,
+          function () {
+            callback ();
+          },
+          function (progress, total) {
+            response.log.progress ('Downloading', progress, total);
+          }
+        );
+      },
 
-    taskExtract: ['taskHttp', function (callback) {
-      var xExtract = require ('xcraft-core-extract');
-      var outDir = path.dirname (outputFile);
+      taskExtract: [
+        'taskHttp',
+        function (callback) {
+          var xExtract = require ('xcraft-core-extract');
+          var outDir = path.dirname (outputFile);
 
-      /* HACK: a very long filename exists in the tarball, then it is a
+          /* HACK: a very long filename exists in the tarball, then it is a
        *       problem for node.js and the 260 chars limitation.
        */
-      xExtract.targz (outputFile, outDir, /very-very-very-long/, response, function (err) {
-        var srcDir = path.join (xcraftConfig.tempRoot,
-                                'src',
-                                'unigw-cf58947c03a304e67a2f283ca1943d0ed3b898d5' /* pkgConfig.name + '-' + pkgConfig.version */);
-        callback (err ? 'extract failed: ' + err : null, srcDir);
-      }, function (progress, total) {
-        response.log.progress ('Extracting', progress, total);
-      });
-    }],
+          xExtract.targz (
+            outputFile,
+            outDir,
+            /very-very-very-long/,
+            response,
+            function (err) {
+              var srcDir = path.join (
+                xcraftConfig.tempRoot,
+                'src',
+                'unigw-cf58947c03a304e67a2f283ca1943d0ed3b898d5' /* pkgConfig.name + '-' + pkgConfig.version */
+              );
+              callback (err ? 'extract failed: ' + err : null, srcDir);
+            },
+            function (progress, total) {
+              response.log.progress ('Extracting', progress, total);
+            }
+          );
+        },
+      ],
 
-    taskPatch: ['taskExtract', function (callback, results) {
-      patchRun (results.taskExtract, response, callback);
-    }],
+      taskPatch: [
+        'taskExtract',
+        function (callback, results) {
+          patchRun (results.taskExtract, response, callback);
+        },
+      ],
 
-    taskMSYS: ['taskPatch', function (callback) {
-      if (xPlatform.getOs () === 'win') {
-        callback (null, xCMake.stripShForMinGW ());
-        return;
+      taskMSYS: [
+        'taskPatch',
+        function (callback) {
+          if (xPlatform.getOs () === 'win') {
+            callback (null, xCMake.stripShForMinGW ());
+            return;
+          }
+          callback ();
+        },
+      ],
+
+      taskCMake: [
+        'taskMSYS',
+        function (callback, results) {
+          cmakeRun (results.taskExtract, response, callback);
+        },
+      ],
+
+      taskMake: [
+        'taskCMake',
+        function (callback, results) {
+          makeRun (
+            path.join (results.taskExtract, '../BUILD_WPKG'),
+            response,
+            callback
+          );
+        },
+      ],
+    },
+    function (err, results) {
+      if (err) {
+        response.log.err (err);
       }
-      callback ();
-    }],
 
-    taskCMake: ['taskMSYS', function (callback, results) {
-      cmakeRun (results.taskExtract, response, callback);
-    }],
-
-    taskMake: ['taskCMake', function (callback, results) {
-      makeRun (path.join (results.taskExtract, '../BUILD_WPKG'), response, callback);
-    }]
-  }, function (err, results) {
-    if (err) {
-      response.log.err (err);
-    }
-
-    /* Restore MSYS path. */
-    if (results.taskMSYS) {
-      for (const p of results.taskMSYS) {
-        xEnv.var.path.insert (p.index, p.location);
+      /* Restore MSYS path. */
+      if (results.taskMSYS) {
+        for (const p of results.taskMSYS) {
+          xEnv.var.path.insert (p.index, p.location);
+        }
       }
-    }
 
-    response.events.send ('wpkg.build.finished');
-  });
+      response.events.send ('wpkg.build.finished');
+    }
+  );
 };
 
 /**
@@ -197,6 +240,6 @@ exports.xcraftCommands = function () {
   const xUtils = require ('xcraft-core-utils');
   return {
     handlers: cmd,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json'))
+    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
   };
 };
