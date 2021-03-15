eea01275f133172427bf4c48c55b8d4ab83276dd
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-03-15 15:48:25 +0100

    Upgrade wpkg (for Windows build) (bis)

--- config.js
+++ config.js
@@ -14,14 +14,14 @@ module.exports = [
     type: 'input',
     name: 'version',
     message: 'version',
-    default: '1372a9d146426ae38b83390750069940794fe2ca',
+    default: '6150a1f295c982dbf86e0954f05d386e646d4cf5',
   },
   {
     type: 'input',
     name: 'src',
     message: 'source URI',
     default:
-      'https://github.com/Xcraft-Inc/wpkg/archive/1372a9d146426ae38b83390750069940794fe2ca.tar.gz',
+      'https://github.com/Xcraft-Inc/wpkg/archive/6150a1f295c982dbf86e0954f05d386e646d4cf5.tar.gz',
   },
   {
     type: 'input',
