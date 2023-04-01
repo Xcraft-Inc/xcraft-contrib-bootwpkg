37b0eff7375809e48f84c3585cf2ac7aba5f8f50
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-04-01 22:47:29 +0200

    Upgrade wpkg

--- config.js
+++ config.js
@@ -14,14 +14,14 @@ module.exports = [
     type: 'input',
     name: 'version',
     message: 'version',
-    default: '5882b1252fb21787fafa8ae06b19cb97b3e66af3',
+    default: '4e6d933248b84a72694e2e63a0461ce54264ce7b',
   },
   {
     type: 'input',
     name: 'src',
     message: 'source URI',
     default:
-      'https://github.com/Xcraft-Inc/wpkg/archive/5882b1252fb21787fafa8ae06b19cb97b3e66af3.tar.gz',
+      'https://github.com/Xcraft-Inc/wpkg/archive/4e6d933248b84a72694e2e63a0461ce54264ce7b.tar.gz',
   },
   {
     type: 'input',
