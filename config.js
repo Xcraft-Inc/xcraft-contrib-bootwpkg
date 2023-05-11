45b48b8de9c33da0e89ea38efc5baf434616dcb6
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-05-11 09:17:53 +0200

    Bump wpkg version

--- config.js
+++ config.js
@@ -14,14 +14,14 @@ module.exports = [
     type: 'input',
     name: 'version',
     message: 'version',
-    default: '4019b13838bdc3249dd25ab9b60ce26438c687f8',
+    default: 'a253ffbf34d2eca2e90b93c85fd08a1dd11e8127',
   },
   {
     type: 'input',
     name: 'src',
     message: 'source URI',
     default:
-      'https://github.com/Xcraft-Inc/wpkg/archive/4019b13838bdc3249dd25ab9b60ce26438c687f8.tar.gz',
+      'https://github.com/Xcraft-Inc/wpkg/archive/a253ffbf34d2eca2e90b93c85fd08a1dd11e8127.tar.gz',
   },
   {
     type: 'input',
